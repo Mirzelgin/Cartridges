964acddb42956d3f3f8451de3c41bc146df7d7d8
Алексей <dubinin.lexa@yandex.ru>
2019-03-04 14:47:39 +0300

    1

--- Cartridges/ViewModel/MainWindowVM.cs
+++ Cartridges/ViewModel/MainWindowVM.cs
@@ -2,28 +2,45 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Collections.ObjectModel;
+using CartridgesNS.Model;
 
 namespace CartridgesNS.ViewModel
 {
     class MainWindowVM : INotifyPropertyChanged
     {
-        ObservableCollection<Model.Cartridges> cartridges;
-        Model.Cartridges cartridgesSelRow;
-        ObservableCollection<Model.Devices> devices;
-        Model.Devices devicesSelRow;
-        ObservableCollection<Model.Locations> locations;
-        Model.Locations locationSelRow;
-        ObservableCollection<Model.MainLog> mainLog;
-        Model.MainLog mainLogSelRow;
+        Model.BaseContext db = new BaseContext();
 
-        public ObservableCollection<Model.Cartridges> Cartridges { get => cartridges; set => cartridges = value; }
-        public Model.Cartridges CartridgesSelRow { get => cartridgesSelRow; set => cartridgesSelRow = value; }
-        public ObservableCollection<Model.Devices> Devices { get => devices; set => devices = value; }
-        public Model.Devices DevicesSelRow { get => devicesSelRow; set => devicesSelRow = value; }
-        public ObservableCollection<Model.Locations> Locations { get => locations; set => locations = value; }
-        public Model.Locations LocationSelRow { get => locationSelRow; set => locationSelRow = value; }
-        public ObservableCollection<Model.MainLog> MainLog { get => mainLog; set => mainLog = value; }
-        public Model.MainLog MainLogSelRow { get => mainLogSelRow; set => mainLogSelRow = value; }
+        private ObservableCollection<Model.Cartridges> cartridges;
+        private Model.Cartridges cartridgesSelRow;
+        private Model.Cartridges cartridgesNewRow;
+        private ObservableCollection<Model.Devices> devices;
+        private Model.Devices devicesSelRow;
+        private Model.Devices devicesNewRow;
+        private ObservableCollection<Model.Locations> locations;
+        private Model.Locations locationSelRow;
+        private Model.Locations locationNewRow;
+        private ObservableCollection<Model.MainLog> mainLog;
+        private Model.MainLog mainLogSelRow;
+        private Model.MainLog mainLogNewRow;
+
+        public ObservableCollection<Cartridges> Cartridges { get => cartridges; set => cartridges = value; }
+        public Cartridges CartridgesSelRow { get => cartridgesSelRow; set => cartridgesSelRow = value; }
+        public Cartridges CartridgesNewRow { get => cartridgesNewRow; set => cartridgesNewRow = value; }
+        public ObservableCollection<Devices> Devices { get => devices; set => devices = value; }
+        public Devices DevicesSelRow { get => devicesSelRow; set => devicesSelRow = value; }
+        public Devices DevicesNewRow { get => devicesNewRow; set => devicesNewRow = value; }
+        public ObservableCollection<Locations> Locations { get => locations; set => locations = value; }
+        public Locations LocationSelRow { get => locationSelRow; set => locationSelRow = value; }
+        public Locations LocationNewRow { get => locationNewRow; set => locationNewRow = value; }
+        public ObservableCollection<MainLog> MainLog { get => mainLog; set => mainLog = value; }
+        public MainLog MainLogSelRow { get => mainLogSelRow; set => mainLogSelRow = value; }
+        public MainLog MainLogNewRow { get => mainLogNewRow; set => mainLogNewRow = value; }
+
+        private RelayCommand addRowCommnad;
+        public MainWindowVM()
+        {
+            MainLog = new ObservableCollection<MainLog>(db.MainLog.Include("Devices"));
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName]string prop="")
--- WpfApp3/Model/Cartridge.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WpfApp3.Model
-{
-    class Cartridge
-    {
-        public int Id { get; set; }
-        public string Description { get; set; }
-    }
-}
